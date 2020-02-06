858f83398832053f69bcfb6e624df8eb2b5d07e1
SadenPain <saschapaezsk8@gmail.com>
2020-02-06 12:35:50 +0100

    more more

--- src/sensor_humedad.ino
+++ src/sensor_humedad.ino
@@ -1,6 +1,11 @@
 #include "DHT.h"
+#include <Servo.h>
 
-#define DHTTYPE DHT11
+Servo horizontal;
+int servo_h = 90;
+
+int servo_h_LimitHigh = 180;
+int servo_h_LimitLow = 65;
 
 /*
 Connect pin 1 (on the left) of the sensor to +5V
@@ -11,16 +16,17 @@ Connect pin 2 of the sensor to whatever your DHTPIN is
 Connect pin 4 (on the right) of the sensor to GROUND
 Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
 */
-
+#define DHTTYPE DHT11
 const int DHTPIN = 5; //What digital pin we're connected to
-
-DHT dht(DHTPin, DHTTYPE);
-
+#ifdef DHTTYPE DHT11
+  DHT dht(DHTPin, DHTTYPE);
+#endif
 void setup(){
   Serial.begin(9600);
   Serial.println("DHT 11 test!");
   // pinMode(DHTPIN, INPUT); se supone que no hace falta
-
+  horizontal.attach(8);
+  horizontal.write(180)
   dht.begin();
 }
 
@@ -42,4 +48,12 @@ void loop(){
   Serial.print("Temperature: ");
   Serial.print(t);
   Serial.print(" *C ");
-}
+
+    if(h < 40){
+      servo_h = servo_h_LimitLow;
+    }else{
+      servo_h = servo_h_LimitLow;
+    }
+    horizontal.write(servo_h);
+
+  }
